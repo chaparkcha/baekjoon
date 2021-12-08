e70225ab947673b400c6a95cbe5a4607682334c4
unknown <dnwn0527@gmail.com>
2021-12-08 22:37:57 +0900

    init

---
+++ src/main/java/beakjoon/Main.java
@@ -0,0 +1,15 @@
+package beakjoon;
+
+import beakjoon.solutions.Problem1026;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new Problem1026().solution());
+    }
+}
---
+++ src/main/java/beakjoon/solutions/Problem1026.java
@@ -0,0 +1,50 @@
+package beakjoon.solutions;
+
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Arrays;
+// 클래스
+public class Problem1026 {
+    public Number solution() {
+        // 5
+        // 1 1 1 6 0
+        // 2 7 8 3 1
+        // A. 18
+
+        // 3
+        // 1 1 3
+        // 10 30 20
+        // A.80
+
+        // 9
+        // 5 15 100 31 39 0 0 3 26
+        // 11 12 13 2 3 4 5 9 1
+        // A.528
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] ArrayA = new int[N];
+        int[] ArrayB = new int[N];
+        for (int i = 0; i < N; i++) {
+            ArrayA[i] = sc.nextInt();
+        }
+        for (int i = 0; i < N; i++) {
+            ArrayB[i] = sc.nextInt();
+ 
+        }
+        int answer = 0;
+        Integer[] A = new Integer[N];
+        Integer[] B = new Integer[N];
+        for(int i=0; i < N; i++) {
+            A[i] = ArrayA[i];
+            B[i] = ArrayB[i];
+        }
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+        for(int i=0; i < N; i++) {
+            answer = answer + A[i]*B[i];
+        }
+        
+        sc.close();
+        return answer;
+    }
+}
